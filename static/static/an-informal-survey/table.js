dc4c66caf33dbfff77ea214f2098400e5ce9072d
Ben Boyter <ben@boyter.org>
2019-09-27 16:23:39 +1000

    Update

--- static/static/an-informal-survey/table.js
+++ static/static/an-informal-survey/table.js
@@ -1,5 +1,31 @@
 $(document).ready( function () {
-    $("table").DataTable({
-    "paging": false,
-  });
+    $(".table-1").next().DataTable({
+        "paging": false,
+        "order": [[ 1, "desc" ]]
+    });
+
+    $(".table-2").next().DataTable({
+        "paging": false,
+        "order": [[ 0, "asc" ]]
+    });
+
+    $(".table-3").next().DataTable({
+        "paging": false,
+        "order": [[ 0, "asc" ]]
+    });
+
+    $(".table-4").next().DataTable({
+        "paging": false,
+        "order": [[ 2, "desc" ]]
+    });
+
+    $(".table-5").next().DataTable({
+        "paging": false,
+        "order": [[ 2, "desc" ]]
+    });
+
+    $(".table-6").next().DataTable({
+        "paging": false,
+        "order": [[ 2, "desc" ]]
+    });
 });
