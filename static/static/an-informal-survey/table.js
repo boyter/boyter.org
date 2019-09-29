f612da75a9998a1aee478dac1569296a68b14456
Ben Boyter <ben@boyter.org>
2019-09-29 17:06:28 +1000

    Update

--- static/static/an-informal-survey/table.js
+++ static/static/an-informal-survey/table.js
@@ -1,31 +1,55 @@
 $(document).ready( function () {
     $(".table-1").next().DataTable({
-        "paging": false,
-        "order": [[ 1, "desc" ]]
+        "paging": true,
+        "order": [[ 1, "desc" ]],
+        "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ]
     });
 
     $(".table-2").next().DataTable({
-        "paging": false,
-        "order": [[ 0, "asc" ]]
+        "paging": true,
+        "order": [[ 0, "asc" ]],
+        "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ]
     });
 
     $(".table-3").next().DataTable({
-        "paging": false,
-        "order": [[ 0, "asc" ]]
+        "paging": true,
+        "order": [[ 0, "asc" ]],
+        "pageLength": 50,
+        "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ]
     });
 
     $(".table-4").next().DataTable({
-        "paging": false,
-        "order": [[ 2, "desc" ]]
+        "paging": true,
+        "order": [[ 2, "desc" ]],
+        "pageLength": 50,
+        "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ]
     });
 
     $(".table-5").next().DataTable({
-        "paging": false,
-        "order": [[ 2, "desc" ]]
+        "paging": true,
+        "order": [[ 2, "desc" ]],
+        "pageLength": 50,
+        "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ]
     });
 
     $(".table-6").next().DataTable({
-        "paging": false,
-        "order": [[ 2, "desc" ]]
+        "paging": true,
+        "order": [[ 2, "desc" ]],
+        "pageLength": 50,
+        "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ]
+    });
+
+    $(".table-7").next().DataTable({
+        "paging": true,
+        "order": [[ 0, "asc" ]],
+        "pageLength": 50,
+        "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ]
+    });
+
+    $(".table-8").next().DataTable({
+        "paging": true,
+        "order": [[ 0, "asc" ]],
+        "pageLength": 50,
+        "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ]
     });
 });
