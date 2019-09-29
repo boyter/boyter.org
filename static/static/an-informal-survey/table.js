e8a0001569dd0815609910486c61a8d06c56ad89
Ben Boyter <ben@boyter.org>
2019-09-30 08:17:40 +1000

    Tweak folder

--- static/static/an-informal-survey/table.js
+++ static/static/an-informal-survey/table.js
@@ -2,12 +2,14 @@ $(document).ready( function () {
     $(".table-1").next().DataTable({
         "paging": true,
         "order": [[ 1, "desc" ]],
+        "pageLength": 25,
         "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ]
     });
 
     $(".table-2").next().DataTable({
         "paging": true,
         "order": [[ 0, "asc" ]],
+        "pageLength": 25,
         "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ]
     });
 
