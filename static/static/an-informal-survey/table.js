888e9503ba62d31eff04213bee9910426b6c07fd
Ben Boyter <ben@boyter.org>
2019-09-29 17:08:10 +1000

    Change page size

--- static/static/an-informal-survey/table.js
+++ static/static/an-informal-survey/table.js
@@ -14,42 +14,42 @@ $(document).ready( function () {
     $(".table-3").next().DataTable({
         "paging": true,
         "order": [[ 0, "asc" ]],
-        "pageLength": 50,
+        "pageLength": 25,
         "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ]
     });
 
     $(".table-4").next().DataTable({
         "paging": true,
         "order": [[ 2, "desc" ]],
-        "pageLength": 50,
+        "pageLength": 25,
         "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ]
     });
 
     $(".table-5").next().DataTable({
         "paging": true,
         "order": [[ 2, "desc" ]],
-        "pageLength": 50,
+        "pageLength": 25,
         "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ]
     });
 
     $(".table-6").next().DataTable({
         "paging": true,
         "order": [[ 2, "desc" ]],
-        "pageLength": 50,
+        "pageLength": 25,
         "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ]
     });
 
     $(".table-7").next().DataTable({
         "paging": true,
         "order": [[ 0, "asc" ]],
-        "pageLength": 50,
+        "pageLength": 25,
         "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ]
     });
 
     $(".table-8").next().DataTable({
         "paging": true,
         "order": [[ 0, "asc" ]],
-        "pageLength": 50,
+        "pageLength": 25,
         "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ]
     });
 });
